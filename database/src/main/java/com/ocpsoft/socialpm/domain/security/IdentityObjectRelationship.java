40e07b1399afd1c5f00101d5561896882f99b7e8
Shane Bryzak <sbryzak@gmail.com>
2011-09-16 16:42:05 -0400

    added idm annotations

--- database/src/main/java/com/ocpsoft/socialpm/domain/security/IdentityObjectRelationship.java
+++ database/src/main/java/com/ocpsoft/socialpm/domain/security/IdentityObjectRelationship.java
@@ -1,5 +1,7 @@
 package com.ocpsoft.socialpm.domain.security;
 
+import static org.jboss.seam.security.annotations.management.EntityType.IDENTITY_RELATIONSHIP;
+
 import java.io.Serializable;
 
 import javax.persistence.Entity;
@@ -8,10 +10,11 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
+import org.jboss.seam.security.annotations.management.IdentityEntity;
 import org.jboss.seam.security.annotations.management.IdentityProperty;
 import org.jboss.seam.security.annotations.management.PropertyType;
 
-@Entity
+@Entity @IdentityEntity(IDENTITY_RELATIONSHIP)
 public class IdentityObjectRelationship implements Serializable
 {
    private static final long serialVersionUID = 4571679866526917176L;
--- database/src/main/java/com/ocpsoft/socialpm/domain/security/IdentityRoleName.java
+++ database/src/main/java/com/ocpsoft/socialpm/domain/security/IdentityRoleName.java
@@ -1,12 +1,16 @@
 package com.ocpsoft.socialpm.domain.security;
 
+import static org.jboss.seam.security.annotations.management.EntityType.IDENTITY_ROLE_NAME;
+
 import java.io.Serializable;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
-@Entity
+import org.jboss.seam.security.annotations.management.IdentityEntity;
+
+@Entity @IdentityEntity(IDENTITY_ROLE_NAME)
 public class IdentityRoleName implements Serializable
 {
    private static final long serialVersionUID = 8775236263787825703L;
